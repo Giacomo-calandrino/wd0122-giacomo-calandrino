6d0d4dc3addaea007dc8558ddd869e47609af029
Giacomo-calandrino <giacomocalandrino@gmail.com>
2022-04-11 18:00:17 +0200

    commit11/04

---
+++ m1/w3/d1/assets/js/index.js
@@ -0,0 +1,4 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+alert('3 - 2');
+window.alert("la lista è elaborata casualmente");
---
+++ m1/w3/d1/assets/js/naming.js
@@ -0,0 +1,32 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+var PetPreferito = 'coniglio';
+
+var $pet = 'giraffa';
+console.log($pet);
+
+document.write($pet + ' ');
+
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2 + ' ');
+
+console.log(_pet2);
+
+//var 12 = 'numero';
+
+var _pet3 = "falco";
+console.log(_pet3);
+document.write(_pet3 + ' ');
+
+
+document.write(petPreferito + ' ');
+document.write(PetPreferito + ' ');
+
+document.getElementById('domestici').innerHTML = 4;
+document.getElementById('selvatici').innerHTML = 3;

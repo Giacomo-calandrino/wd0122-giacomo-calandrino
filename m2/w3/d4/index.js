001e46bde69e5af3aefa252cc3f2a13db091509f
Giacomo-calandrino <giacomocalandrino@gmail.com>
2022-05-12 17:05:22 +0200

    12/05

---
+++ m2/w3/d4/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) => {
+   res.sendFile(__dirname + '/index.html');
+})
+
+app.get('/contatti', (req,res) => {
+    res.sendFile(__dirname + '/contatti.html')
+})
+
+app.get('/chisiamo', (req,res) => {
+    res.sendFile(__dirname + '/chisiamo.html')
+})
+
+app.listen(3000, () => {
+    console.log('Server running at http://127.0.0.1:3000')
+})
